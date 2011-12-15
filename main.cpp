48a410902530c1545f3f044c8a5066a9e69e1545
Ian Clarkson <clarksonian@gmail.com>
2011-12-14 22:56:22 -0500

    Iterating through the parameters, sending a MIDI note to the instrument, and saving the output as a .WAV file. Works!

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 // VST
 #define WIN32 // to import the right types
 #include "pluginterfaces/vst2.x/aeffectx.h"
+
 typedef AEffect* (*PluginEntryProc)(audioMasterCallback audioMaster);
 
 // globals
@@ -24,43 +25,43 @@ static AEffect* effect_;
 // put our good stuff here -- write out what we want when ASIO requests it
 static void MyAudioCallback(int index)
 {
-  // our location within the sound to play back
-  static int buf_idx = 0;
+  //// our location within the sound to play back
+  //static int buf_idx = 0;
 
-  // go through the buffers, only using outputs
-  ASIOBufferInfo*  b = asio_.buffer_infos_  + asio_.input_channels_;
-  ASIOChannelInfo* c = asio_.channel_infos_ + asio_.input_channels_;
-  for (int i = 0; i < asio_.output_channels_; ++i, ++b, ++c)
-  {
-    switch (c->type)
-    {
-    case ASIOSTInt32LSB:
-      {
-        int32_t* int32_p = static_cast<int32_t*>(b->buffers[index]);
-        for (int j = 0; j < asio_.buffer_size_; ++j)
-        {
-          // random data
-          float smp = static_cast<float>(rand()) / RAND_MAX;
+  //// go through the buffers, only using outputs
+  //ASIOBufferInfo*  b = asio_.buffer_infos_  + asio_.input_channels_;
+  //ASIOChannelInfo* c = asio_.channel_infos_ + asio_.input_channels_;
+  //for (int i = 0; i < asio_.output_channels_; ++i, ++b, ++c)
+  //{
+  //  switch (c->type)
+  //  {
+  //  case ASIOSTInt32LSB:
+  //    {
+  //      int32_t* int32_p = static_cast<int32_t*>(b->buffers[index]);
+  //      for (int j = 0; j < asio_.buffer_size_; ++j)
+  //      {
+  //        // random data
+  //        float smp = static_cast<float>(rand()) / RAND_MAX;
 
-          // read from sound file
-          // take the corresponding channel index if it exists
-          int channel = std::min(static_cast<unsigned int>(j), snd_.data.size() - 1);
-          //float smp = snd_.data[channel][(buf_idx + j) % snd_.data[channel].size()];
-          
-          // place the output
-          *int32_p++ = static_cast<int32_t>(smp * (smp >= 0 ? INT32_MAX : INT32_MIN));
-        }
-      }
-      break;
-    default:
-      break;
-    }
-  }
+  //        // read from sound file
+  //        // take the corresponding channel index if it exists
+  //        int channel = std::min(static_cast<unsigned int>(j), snd_.data.size() - 1);
+  //        //float smp = snd_.data[channel][(buf_idx + j) % snd_.data[channel].size()];
+  //        
+  //        // place the output
+  //        *int32_p++ = static_cast<int32_t>(smp * (smp >= 0 ? INT32_MAX : INT32_MIN));
+  //      }
+  //    }
+  //    break;
+  //  default:
+  //    break;
+  //  }
+  //}
 
-  // adjust the location within the sound for next output
-  // assume that each channel contains the same amount of info
-  buf_idx += asio_.buffer_size_;
-  //buf_idx %= snd_.data[0].size();
+  //// adjust the location within the sound for next output
+  //// assume that each channel contains the same amount of info
+  //buf_idx += asio_.buffer_size_;
+  ////buf_idx %= snd_.data[0].size();
 }
 
 VstIntPtr VSTCALLBACK HostCallback(AEffect* effect, VstInt32 opcode, VstInt32 index, VstIntPtr value, void* ptr, float opt)
@@ -110,15 +111,72 @@ int main(int argc, char* argv[])
   ret = effect_->dispatcher(effect_, effSetBlockSize, 0, asio_.buffer_size_, 0, 0);
   if (ret != 0) exit(EXIT_FAILURE);
 
+  // iterate parameters
+	for (VstInt32 i = 0; i < effect_->numParams; i++)
+	{
+		char paramName[256] = {0};
+		char paramLabel[256] = {0};
+		char paramDisplay[256] = {0};
+
+		effect_->dispatcher(effect_, effGetParamName, i, 0, paramName, 0);
+		effect_->dispatcher(effect_, effGetParamLabel, i, 0, paramLabel, 0);
+		effect_->dispatcher(effect_, effGetParamDisplay, i, 0, paramDisplay, 0);
+		float value = effect_->getParameter(effect_, i);
+
+		printf("Param %03d: %s [%s %s] (normalized = %f)\n", i, paramName, paramDisplay, paramLabel, value);
+	}
+ 
+  VstMidiEvent midi_event = { 0 };
+  midi_event.type = kVstMidiType;
+  midi_event.byteSize = sizeof(VstMidiEvent);
+  
+  // set the midi data
+  midi_event.midiData[0] = (char)0x90; // channel 1 note on
+  midi_event.midiData[1] = 60;   // C4
+  midi_event.midiData[2] = 127;  // max velocity
+    
+  midi_event.flags = kVstMidiEventIsRealtime;
+
+  VstEvents ev = { 0 };
+  ev.numEvents = 1;
+  ev.events[0] = reinterpret_cast<VstEvent*>(&midi_event);
+
   // resume
+  float    sample_rate = 44100;
+  VstInt32 block_size  = 10*sample_rate;
+
+  effect_->dispatcher(effect_, effSetSampleRate, 0, NULL, NULL, sample_rate);
+  effect_->dispatcher(effect_, effSetBlockSize, 0, block_size, NULL, 0.f);
+
 	effect_->dispatcher(effect_, effMainsChanged, 0, 1, 0, 0);
+
+  // set up outputs
+  Sound<float> output;
+  output.data.resize(2);
+  output.data[0] = output.data[1] = std::vector<float>(block_size, 0);
+
+  float* output_pass[2];
+  output_pass[0] = &(*output.data[0].begin());
+  output_pass[1] = &(*output.data[1].begin());
+
+  // call once to clear buffers
+  effect_->processReplacing(effect_, NULL, output_pass, block_size);
+
+  // try to call it
+
+  //output.ReadWav("output.wav");
+
+  // write our output
+
+  // infinite loop!
+  //for (;;)
+  {
+    effect_->dispatcher(effect_, effProcessEvents, 0, 0, &ev, 0.f);
+    effect_->processReplacing(effect_, NULL, output_pass, block_size);
+  }
+
+  output.WriteWav("output.wav");
 
   // suspend
 	effect_->dispatcher(effect_, effMainsChanged, 0, 0, 0, 0);
-
-  // infinite loop!
-  for (;;)
-  {
-    int n = 0;
-  }
 }
