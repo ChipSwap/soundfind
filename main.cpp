29d5b3ec60e4bd1486c194c0e78c6632975e452d
Ian Clarkson <clarksonian@gmail.com>
2011-12-21 17:35:13 -0500

    Now constantly playing the best matching sound.

--- main.cpp
+++ main.cpp
@@ -7,52 +7,62 @@
 // this is static global so we can access it inside the audio callback
 #include <algorithm> // for std::min
 #include <fstream> // for if/ofstream
+#include <Windows.h> // for mutexes
 
 // globals
-static Sound<float> snd_;
+static CRITICAL_SECTION snd_crit_;
+
+static Sound<float> snd_;        // our loaded sound
+static Sound<float> best_match_; // our best match
+
+static int buf_idx_ = 0;
 static ASIOHelper asio_;
 static VSTHelper vst_;
 
 // put our good stuff here -- write out what we want when ASIO requests it
 static void MyAudioCallback(int index)
 {
-  //// our location within the sound to play back
-  //static int buf_idx = 0;
+  EnterCriticalSection(&snd_crit_);
 
-  //// go through the buffers, only using outputs
-  //ASIOBufferInfo*  b = asio_.buffer_infos_  + asio_.input_channels_;
-  //ASIOChannelInfo* c = asio_.channel_infos_ + asio_.input_channels_;
-  //for (int i = 0; i < asio_.output_channels_; ++i, ++b, ++c)
-  //{
-  //  switch (c->type)
-  //  {
-  //  case ASIOSTInt32LSB:
-  //    {
-  //      int32_t* int32_p = static_cast<int32_t*>(b->buffers[index]);
-  //      for (int j = 0; j < asio_.buffer_size_; ++j)
-  //      {
-  //        // random data
-  //        float smp = static_cast<float>(rand()) / RAND_MAX;
+  // choose which sound to play
+  const Sound<float>& s = best_match_;
 
-  //        // read from sound file
-  //        // take the corresponding channel index if it exists
-  //        int channel = std::min(static_cast<unsigned int>(j), snd_.data.size() - 1);
-  //        //float smp = snd_.data[channel][(buf_idx + j) % snd_.data[channel].size()];
-  //        
-  //        // place the output
-  //        *int32_p++ = static_cast<int32_t>(smp * (smp >= 0 ? INT32_MAX : INT32_MIN));
-  //      }
-  //    }
-  //    break;
-  //  default:
-  //    break;
-  //  }
-  //}
+  // go through the buffers, only using outputs
+  ASIOBufferInfo*  b = asio_.buffer_infos_  + asio_.input_channels_;
+  ASIOChannelInfo* c = asio_.channel_infos_ + asio_.input_channels_;
+  for (int i = 0; i < asio_.output_channels_; ++i, ++b, ++c)
+  {
+    switch (c->type)
+    {
+    case ASIOSTInt32LSB:
+      {
+        int32_t* int32_p = static_cast<int32_t*>(b->buffers[index]);
+        for (int j = 0; j < asio_.buffer_size_; ++j)
+        {
+          // random data
+          //float smp = static_cast<float>(rand()) / RAND_MAX;
 
-  //// adjust the location within the sound for next output
-  //// assume that each channel contains the same amount of info
-  //buf_idx += asio_.buffer_size_;
-  ////buf_idx %= snd_.data[0].size();
+          // read from sound file
+          // take the corresponding channel index if it exists
+          int channel = std::min(static_cast<unsigned int>(j), s.data_.size() - 1);
+          float smp = s.data_[channel][(buf_idx_ + j) % s.data_[channel].size()];
+          
+          // place the output
+          *int32_p++ = static_cast<int32_t>(smp * (smp >= 0 ? INT32_MAX : INT32_MIN));
+        }
+      }
+      break;
+    default:
+      break;
+    }
+  }
+
+  LeaveCriticalSection(&snd_crit_);
+
+  // adjust the location within the sound for next output
+  // assume that each channel contains the same amount of info
+  buf_idx_ += asio_.buffer_size_;
+  buf_idx_ %= s.data_[0].size();
 }
 
 template <class T>
@@ -71,6 +81,9 @@ int main(int argc, char* argv[])
   if (argc < 2)
     exit(EXIT_FAILURE);
 
+  // make a critical section for sound playing
+  InitializeCriticalSection(&snd_crit_);
+
   // need a synth to load
   std::string vst_path = argv[1];
 
@@ -88,15 +101,19 @@ int main(int argc, char* argv[])
   unsigned int snd_len = snd_.data_[0].size();
 
   // get some output
+  best_match_.data_ = std::vector< std::vector<float> >(2, std::vector<float>(snd_len, 0));
+  
+  // get pointers to generate the output
   float* output[2];
-  output[0] = new float[snd_len];
-  output[1] = new float[snd_len];
+  output[0] = &(*best_match_.data_[0].begin());
+  output[1] = &(*best_match_.data_[1].begin());
 
   vst_.GenerateOutput(snd_len, output);
 
   // check the difference
   float diff = CalculateSonicDifference(snd_, output);
 
-  delete[] output[0];
-  delete[] output[1];
+  for (;;) {}
+
+  DeleteCriticalSection(&snd_crit_);
 }
